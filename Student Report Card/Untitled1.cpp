9625939081c1cbebd5c6557f9ef436ae9d4db25c
irfan-ahmed24 <irfanahmed99989@gmail.com>
2025-01-07 20:23:25 +0600

    adding

--- Student Report Card/Untitled1.cpp
+++ Student Report Card/Untitled1.cpp
@@ -1,58 +1,30 @@
 #include<iostream>
 using namespace std;
-
-class Student {
-    string name;
-    int rollNumber;
-    float marks[3];
+class A{
 public:
-    Student(string N, int RN, float M[3]);
-    float calculateAverage();
-    void displayDetails();
+    virtual void show()=0;
 };
-
-Student::Student(string N, int RN, float M[3]) {
-    name = N;
-    rollNumber = RN;
-    for (int i = 0; i < 3; i++) {
-        marks[i] = M[i];
+class B:public A{
+public:
+    void show(){
+        cout<<"class B"<<endl;
     }
-}
-
-float Student::calculateAverage() {
-    float total = 0;
-    for (int i = 0; i < 3; i++) {
-        total += marks[i];
+};
+class C:public A{
+public:
+    void show(){
+        cout<<"class C"<<endl;
     }
-    return total / 3;
-}
-
-void Student::displayDetails() {
-    cout << "Name: " << name << endl;
-    cout << "ID: " << rollNumber << endl;
-    for (int i = 0; i < 3; i++) {
-        cout << "Mark " << i + 1 << ": " << marks[i] << endl;
-    }
-    cout << "Average marks: " << calculateAverage() << endl;
-}
-
-int main() {
-    string name;
-    int rollNumber;
-    float marks[3];
-
-    cout << "Enter student name: ";
-    getline(cin, name);
-    cout << "Enter roll number: ";
-    cin >> rollNumber;
-
-    for (int i = 0; i < 3; i++) {
-        cout << "Enter mark " << i + 1 << ": ";
-        cin >> marks[i];
-    }
-    cout<<endl;
-    Student ob(name, rollNumber, marks);
-    ob.displayDetails();
-
+};
+int main(){
+    cout<<"Irfan Ahmked"<<endl;
+    A *p[2];
+    B m;
+    C n;
+    p[0]=&m;
+    p[1]=&n;
+    p[0]->show();
+    p[1]->show();
     return 0;
+
 }
